eebd08346252b9d70bf27768ab395828396c21ba
Daniel Tadesse <dtadesse@Daniels-MacBook-Pro-2.local>
2025-11-26 15:13:24 -0500

    TrappingRainWater

---
+++ StringsAndArrays/TrappingRainWater/SolutionTest.java
@@ -0,0 +1,23 @@
+package TrappingRainWater;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SolutionTest  {
+
+    @Test
+    public void trapTestOne() {
+        Solution solution = new Solution();
+        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
+        assertEquals(6, solution.trap(height));
+    }
+
+    @Test
+    public void trapTestTwo() {
+        Solution solution = new Solution();
+        int[] height = {4,2,0,3,2,5};
+        assertEquals(9, solution.trap(height));
+    }
+
+}
