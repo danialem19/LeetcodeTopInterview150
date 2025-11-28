aa1ee52539bac3b0cb12450ed9138c95e8c7772a
Daniel Tadesse <dtadesse@Daniels-MacBook-Pro-2.local>
2025-11-28 06:07:26 -0500

    LengthOfLastWord

--- StringsAndArrays/LengthOfLastWord/Solution.java
+++ StringsAndArrays/LengthOfLastWord/Solution.java
@@ -2,15 +2,15 @@ package LengthOfLastWord;
 
 public class Solution {
     public int lengthOfLastWord(String s) {
-        StringBuilder sb = new StringBuilder();
         int maxLen = Integer.MIN_VALUE;
+        int curCount = 0;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == ' ') {
-                sb = new StringBuilder();
+                curCount = 0;
                 continue;
             }
-            sb.append(s.charAt(i));
-            maxLen = Math.max(maxLen, sb.length());
+            curCount++;
+            maxLen = Math.max(maxLen, curCount);
         }
         return maxLen;
     }
