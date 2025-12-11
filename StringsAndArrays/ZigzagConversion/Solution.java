196d55ae1f30ec18579024abb790eeda3e9586fd
Daniel Tadesse <dtadesse@Daniels-MacBook-Pro-2.local>
2025-12-11 03:26:22 -0500

    ZigzagConversion refactoring

--- StringsAndArrays/ZigzagConversion/Solution.java
+++ StringsAndArrays/ZigzagConversion/Solution.java
@@ -3,44 +3,47 @@ package ZigzagConversion;
 import java.util.Arrays;
 
 public class Solution {
+
     public String convert(String s, int numRows) {
+        if(numRows == 1) return s;
+
         //Assume you are doing this on a 2D matrix, and moving in zigzag
         int n = s.length();
-        int charsPerSection = 2 * numRows - 2;
-        int colsPerSection = numRows - 1;
-        int numSections = Math.ceilDiv(n, charsPerSection);
-        int numCols = colsPerSection * numSections;
+        int charPerSection = 2 * numRows - 2;
+        int numOfSctions = Math.ceilDiv(n, charPerSection);
+        int numOfColPerSection = numRows - 1;
+        int numCols = numOfColPerSection * numOfSctions;
 
-        char[][] matrix  = new char[numRows][numCols];
-        for (char[] rows : matrix) {
-            Arrays.fill(rows, ' ');
+        char[][] matrix = new char[numRows][numCols];
+        for(char[] row : matrix) {
+            Arrays.fill(row, ' ');
         }
 
-        int ver = 0, hor = 0, sIndex = 0;
-        while (sIndex <  n) {
+        int i = 0, rowIndex = 0, colIndex = 0;
+        while(i < n) {
             //Fill the string characters array down
-            while (ver < numRows && sIndex < n) {
-                matrix[ver++][hor] = s.charAt(sIndex++);
+            while(rowIndex < numRows && i < n) {
+                matrix[rowIndex++][colIndex] = s.charAt(i++);
             }
 
             //Fill the string characters array diagonal
-            ver -= 2;
-            hor++;
-            while (ver > 0 && hor < numCols && sIndex < n) {
-                matrix[ver--][hor++] = s.charAt(sIndex++);
+            rowIndex -= 2;
+            colIndex++;
+            while(rowIndex > 0 && colIndex < numCols && i < n) {
+                matrix[rowIndex--][colIndex++] = s.charAt(i++);
             }
         }
 
+        //Create the string
         StringBuilder sb = new StringBuilder();
-        for (char[] rows : matrix) {
-            for (char c : rows) {
-                if (!Character.isSpaceChar(c)) {
+        for(char[] row : matrix) {
+            for(char c : row) {
+                if(c != ' ') {
                     sb.append(c);
                 }
             }
         }
 
         return sb.toString();
-
     }
 }
